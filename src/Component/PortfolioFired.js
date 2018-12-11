9be5777bd8c9591054cce40cdb88005a16bcf24d
GirlSuzette <trinitysuzette_marpis@hotmail.com>
2018-12-10 19:47:33 -0600

    Portafolio One1

--- src/Component/PortfolioFired.js
+++ src/Component/PortfolioFired.js
@@ -8,7 +8,7 @@ class PortfolioFired extends Component {
             <div className="portfolio-fixed">
                 <img src="https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/13310576_10206411498826563_2618225755018554252_n.jpg?_nc_cat=105&_nc_ht=scontent-dfw5-1.xx&oh=34cfc665a90912a84f386e9d82aec02a&oe=5CA27809" />
                 <h1>Cindy S. Mulato</h1>
-                <h4>Developer</h4>
+                <p>Developer</p>
             </div>
         );
     }
